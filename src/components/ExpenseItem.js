68bd0bfd2a99910d9929917108a4ecfc24f6c541
Jeff Ripke <jripke74@gmail.com>
2022-05-20 08:41:13 -0700

    add html to ExpanseItem() component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 function ExpenseItem() {
-  return <h2>Expense Item!</h2>
+  return (
+    <div>
+      <div>March 11th 2020</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$249.67</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
