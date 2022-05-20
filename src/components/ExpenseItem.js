628c2448cfc1e27898ac377da73ab6d42b4689cf
Jeff Ripke <jripke74@gmail.com>
2022-05-20 12:51:27 -0700

    add month, year, day variables

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,17 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+  const month = props.date.toLocaleString('en-us', { month: 'long' });
+  const year = props.date.getFullYear();
+  const day = props.date.toLocaleString('en-us', { day: '2-digit' });
+
   return (
     <div className="expense-item">
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
