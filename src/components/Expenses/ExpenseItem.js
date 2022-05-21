01bc79955095f2d135f43cf06fb947bd82eb6512
Jeff Ripke <jripke74@gmail.com>
2022-05-21 16:22:13 -0700

    add onClick handler to ExpenseItem.js

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,7 +3,11 @@ import Card from '../UI/Card';
 import './ExpenseItem.css';
 import '../UI/Card.css';
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    console.log('Click!!!')
+  };
+  
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -11,6 +15,7 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
