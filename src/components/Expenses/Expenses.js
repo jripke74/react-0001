9f346279821d7770137ba896fead3211816eda28
Jeff Ripke <jripke74@gmail.com>
2022-06-02 16:20:02 -0700

    add ExpensesFilterMonth components

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,18 +3,27 @@ import React, { useState } from 'react';
 import Card from '../UI/Card';
 import ExpensesList from './ExpensesList';
 import ExpensesFilterYear from './ExpensesFilterYear';
+import ExpensesFilterMonth from './ExpensesFilterMonth';
 import ExpensesChart from './ExpensesChart';
 import './Expenses.css';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
+  const [filteredMonth, setFilteredMonth] = useState('January');
 
-  const filterChangeHandler = (selectedYear) => {
+  const filterChangeHandlerYear = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
 
+  const filterChangeHandlerMonth = (selectedMonth) => {
+    setFilteredMonth(selectedMonth);
+  };
+
   const filteredExpenses = props.items.filter((expense) => {
-    return expense.date.getFullYear().toString() === filteredYear;
+    return (
+      expense.date.getFullYear().toString() === filteredYear &&
+      expense.date.toLocaleString('en-us', { month: 'long' }) === filteredMonth
+    );
   });
 
   return (
@@ -22,7 +31,11 @@ const Expenses = (props) => {
       <Card className="expenses">
         <ExpensesFilterYear
           selected={filteredYear}
-          onChangeFilter={filterChangeHandler}
+          onChangeFilter={filterChangeHandlerYear}
+        />
+        <ExpensesFilterMonth
+          selected={filteredMonth}
+          onChangeFilter={filterChangeHandlerMonth}
         />
         <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
